35076fe730040f8f9e819ae0c56e8923eb3cd776
atomduan <atomduan@gmail.com>
2018-12-25 20:06:29 +0800

    add

--- python/linexe.py
+++ python/linexe.py
@@ -5,5 +5,5 @@ def process_line(line):
     print line
 
 if __name__ == '__main__':
-    for l in sys.stdin.readlines():
-        process_line(l.strip())
+    for line in sys.stdin.readlines():
+        process_line(line.strip())
