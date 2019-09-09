5154e566d59f81dde997b07b1f0d1c97feda53be
duanjuntao <duanjuntao@xiaomi.com>
2019-09-09 11:56:53 +0800

    add more example

--- python/date_process.py
+++ python/date_process.py
@@ -8,11 +8,15 @@ from datetime import datetime
 def process_line(line):
     params = line.split('\t');
     create_time = to_date(long(params[16]))
+    can_settle = to_cansettle(int(params[18]))
     print create_time 
 
 def to_date(timestamp):
     ts = datetime.fromtimestamp(timestamp/1000)
     return ts.strftime('%Y-%m-%d_%H:%M:%S')
+
+def to_cansettle(cs):
+    return 'YES' if cs==1 else 'NO'
 
 if __name__ == '__main__':
     file = '~/tmp/res.list.unic'
