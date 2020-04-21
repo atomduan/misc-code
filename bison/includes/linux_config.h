5f814841040698339ecb221f10d42f02f2348247
juntaoduan <atomduan@gmail.com>
2020-04-21 17:29:29 +0800

    add

--- bison/includes/linux_config.h
+++ bison/includes/linux_config.h
@@ -1,5 +1,5 @@
-#ifndef _CONFIG_H_INCLUDED_
-#define _CONFIG_H_INCLUDED_
+#ifndef _LINUX_CONFIG_H_INCLUDED_
+#define _LINUX_CONFIG_H_INCLUDED_
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
@@ -7,6 +7,9 @@
 
 #define _FILE_OFFSET_BITS  64
 
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <stdarg.h>
 #include <stddef.h>
@@ -21,11 +24,30 @@
 #include <grp.h>
 #include <dirent.h>
 #include <glob.h>
+#include <sys/vfs.h>
+
+#include <sys/uio.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/resource.h>
+#include <sched.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/un.h>
 
 #include <time.h>
 #include <malloc.h>
 #include <limits.h>
+#include <sys/ioctl.h>
+#include <crypt.h>
+#include <sys/utsname.h>
 #include <stdbool.h>
 
 /* use of dirname */
@@ -33,4 +55,4 @@
 #include <dlfcn.h>
 
 extern char **environ;
-#endif /* _CONFIG_H_INCLUDED_ END */
+#endif /* _LINUX_CONFIG_H_INCLUDED_ */
