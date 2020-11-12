72f37e7b8d4621b479c0823b2e9f2bd494fa5f0a
juntaoduan <atomduan@gmail.com>
2020-11-12 17:25:07 +0800

    mod

--- sql/payment.sql
+++ sql/payment.sql
@@ -5,7 +5,7 @@ CREATE TABLE `user_deduct_info_wechat` (
       `extra_info` varchar(1024) DEFAULT NULL,
       `create_time` bigint(20) NOT NULL DEFAULT '0',
       `update_time` bigint(20) NOT NULL DEFAULT '0',
-      `mi_id` bigint(20) NOT NULL DEFAULT '0',
+      `dog_id` bigint(20) NOT NULL DEFAULT '0',
       `openid` varchar(1024) DEFAULT NULL,
       `contract_code` varchar(1024) DEFAULT NULL,
       `contract_id` varchar(1024) DEFAULT NULL,
@@ -18,8 +18,8 @@ alter table user_deduct_info add column (
     `channel` tinyint(4) unsigned NOT NULL DEFAULT '0' COMMENT '支付渠道 1.支付宝 2.微信 4.小米钱包',
     is_deleted int(32) unsigned NOT NULL DEFAULT '0' COMMENT '渠道状态,是否已经被删除（禁用）０.否，1.是'
 );
-alter table user_deduct_info add CONSTRAINT `uk_mi_id_channel` UNIQUE (`mi_id`, `channel`);
-alter table user_deduct_info drop index uk_mi_id;
+alter table user_deduct_info add CONSTRAINT `uk_dog_id_channel` UNIQUE (`dog_id`, `channel`);
+alter table user_deduct_info drop index uk_dog_id;
 
 use mofi_instra_payment;
 update deduct_trade_detail set channel=4 where channel=0 and id>4998 and id<9999;
