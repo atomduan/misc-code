864ed2c37cc4f9a7a104e7633db3f446a81aebb5
juntaoduan <atomduan@gmail.com>
2021-07-08 14:58:00 +0800

    add

---
+++ shell/parse_opt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ":option_name" no argument value option
+# "option_name:" option with argument value
+# example: ./cmd -hp abc -f file_foo 
+while getopts :help:hp:f: opt ; do
+    case "$opt" in
+        h|help) echo "help desc" 
+            ;;
+        p) echo "opt p --> ${OPTARG}"
+            ;;
+        f) echo "opt f --> ${OPTARG}"
+            ;;
+    esac
+done
